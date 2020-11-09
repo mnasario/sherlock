012b2593163ad9b7690f4ee058dde213d957ebe6
Marcos Nasario <marcos.nasario@akelius.de>
2020-11-09 19:38:08 +0100

    Handling exception

--- src/main/java/com/sherlock/game/challenge/controller/ChallengeRestController.java
+++ src/main/java/com/sherlock/game/challenge/controller/ChallengeRestController.java
@@ -6,9 +6,11 @@ import com.sherlock.game.challenge.domain.ChallengeSummary;
 import com.sherlock.game.challenge.service.ChallengeService;
 import com.sherlock.game.core.domain.Player;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @AllArgsConstructor
@@ -20,24 +22,53 @@ public class ChallengeRestController {
     @CrossOrigin
     @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<ChallengeRoom> insert(@RequestBody ChallengeConfig config) {
-        return ResponseEntity.ok(challengeService.insert(config));
+
+        try {
+
+            return ResponseEntity.ok(challengeService.insert(config));
+
+        } catch (IllegalArgumentException ex) {
+
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, ex.getMessage(), ex);
+        }
     }
 
     @CrossOrigin
     @GetMapping(path = "/{gameId}/room", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<ChallengeRoom> getRoom(@PathVariable("gameId") String gameId) {
-        return ResponseEntity.ok(challengeService.getRoom(gameId));
+
+        try {
+
+            return ResponseEntity.ok(challengeService.getRoom(gameId));
+
+        } catch (IllegalArgumentException ex) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, ex.getMessage(), ex);
+        }
     }
 
     @CrossOrigin
     @GetMapping(path = "/{gameId}/summary", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<ChallengeSummary> getSummary(@PathVariable("gameId") String gameId) {
-        return ResponseEntity.ok(challengeService.getSummary(gameId));
+
+        try {
+
+            return ResponseEntity.ok(challengeService.getSummary(gameId));
+
+        } catch (IllegalArgumentException ex) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, ex.getMessage(), ex);
+        }
     }
 
     @CrossOrigin
     @GetMapping(path = "/{gameId}/player", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Player> getPlayer(@PathVariable("gameId") String gameId, @RequestParam("player") String playerName) {
-        return ResponseEntity.ok(challengeService.getPlayer(gameId, playerName));
+
+        try {
+
+            return ResponseEntity.ok(challengeService.getPlayer(gameId, playerName));
+
+        } catch (IllegalArgumentException ex) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, ex.getMessage(), ex);
+        }
     }
 }
